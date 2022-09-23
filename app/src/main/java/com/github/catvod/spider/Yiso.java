b4fda0ed7cca276aa5c31fb012bcc49141bc43d4
陌路 <530861555@qq.com>
2022-09-23 19:38:34 +0800

    Update Yiso.java

--- app/src/main/java/com/github/catvod/spider/Yiso.java
+++ app/src/main/java/com/github/catvod/spider/Yiso.java
@@ -1,42 +1,16 @@
 package com.github.catvod.spider;
 
-import android.content.Context;
-
-import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
 import com.github.catvod.utils.okhttp.OkHttpUtil;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.List;
 import java.util.regex.Pattern;
 
-public class Yiso extends Spider {
+public class Yiso extends PushAgent {
     private static final Pattern aliyun = Pattern.compile("(https://www.aliyundrive.com/s/[^\"]+)");
-    private PushAgent pushAgent;
-
-    public String detailContent(List<String> list) {
-        try {
-            return pushAgent.detailContent(list);
-        } catch (Exception e) {
-            SpiderDebug.log(e);
-        }
-        return "";
-    }
-
-    @Override
-    public void init(Context context) {
-        super.init(context);
-        pushAgent = new PushAgent();
-        pushAgent.init(context);
-    }
-
-    public String playerContent(String str, String str2, List<String> list) {
-        return pushAgent.playerContent(str, str2, list);
-    }
 
     protected static HashMap<String, String> sHeaders() {
         HashMap<String, String> headers = new HashMap<>();
@@ -47,9 +21,10 @@ public class Yiso extends Spider {
 
     private Pattern regexVid = Pattern.compile("(\\S+)");
 
+    @Override
     public String searchContent(String key, boolean quick) {
         try {
-            HashMap hashMap = new HashMap();
+            fetchRule(false, 0);
             HashMap<String, String> LT = sHeaders();
             String url = "https://yiso.fun/api/search?name=" + URLEncoder.encode(key) + "&from=ali";
 
@@ -59,7 +34,6 @@ public class Yiso extends Spider {
 
             JSONObject result = new JSONObject();
             JSONArray videos = new JSONArray();
-
             for (int i = 0; i < list.length(); i++) {
                 JSONObject jSONObject = list.getJSONObject(i);
                 String sourceName = jSONObject.getJSONArray("fileInfos").getJSONObject(0).getString("fileName");
@@ -67,10 +41,10 @@ public class Yiso extends Spider {
                 String id = jSONObject.getString("url");
 
                 JSONObject v = new JSONObject();
-                String cover = "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000";
+                String cover = "https://f.haocew.com/image/tv/yiso.jpg";
                 v.put("vod_name", sourceName);
                 v.put("vod_remarks", remark);
-                v.put("vod_id", id);
+                v.put("vod_id", id + "$$$" + cover + "$$$" + sourceName);
                 v.put("vod_pic", cover);
                 videos.put(v);
             }
